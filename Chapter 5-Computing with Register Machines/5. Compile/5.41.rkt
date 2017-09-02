23d927912cba7360c42ca9de8c2ed0b311facf9a
Phoenine <562818991@qq.com>
2017-09-02 10:52:10 +0800

    add ex-5.41: find variable address in compile-env

--- Chapter 5-Computing with Register Machines/5. Compile/5.41.rkt
+++ Chapter 5-Computing with Register Machines/5. Compile/5.41.rkt
@@ -1,3 +1,17 @@
+; SICP exercise 5.41
+;
+; Implement a procedure find-variable, which takes a var and compile-env
+; as parameters, and will return a lexical address if var in compile-env
+; or symbol 'not-found if not.
+
+; instances:
+; (find-variable 'c '((y z) (a b c d e) (x y)))
+; (1 2)
+; (find-variable 'x '((y z) (a b c d e) (x y)))
+; (2 0)
+; (find-variable 'w '((y z) (a b c d e) (x y)))
+; not-found
+
 (define (find-variable var compile-env)
   (define (search-frame frame-ref env)
     (if (null? env)
