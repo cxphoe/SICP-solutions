0f5a1c5960da1da0211a6e961b619fac70a12ded
Phoenine <562818991@qq.com>
2017-09-02 10:53:23 +0800

    add ex-5.42: adopt lexical address in compiler

--- Chapter 5-Computing with Register Machines/5. Compile/5.42.rkt
+++ Chapter 5-Computing with Register Machines/5. Compile/5.42.rkt
@@ -1,41 +1,43 @@
-(define (compile-variable exp target linkage compile-env)
-  (let ((address (find-variable exp compile-env)))
-    (end-with-linkage linkage
-                      (make-instruction-sequence
-                       '(env) (list target)
-                       (if (eq? address 'not-found)
-                           (list (list 'assign
-                                       target
-                                       '(op lookup-variable-value)
-                                       (list 'const exp)
-                                       '(reg env)))
-                           (list (list 'assign
-                                       target
-                                       '(op lexical-address-lookup)
-                                       (list 'const address)
-                                       '(reg env))))))))
+; SICP exercise 5.42
+;
+; Rewrite compile-variable and compile-assignment to adopt the lexical-
+; address by using find-vairable defined in exercise 5.40. If procedure
+; find-variable return symbol 'not-found, then just lookup the variable
+; in the-global-environment.
 
+; Basic concept: set the var to the address and change relevant op so
+; there's no need to repeat the code.
+
+; variable
+(define (compile-variable exp target linkage compile-env)
+  (let ((address (find-variable exp compile-env))
+        (op 'lookup-variable-value))
+    (if (not (eq? address 'not-found))
+        (begin (set! op 'lexical-address-lookup)
+               (set! exp address)))
+    (end-with-linkage linkage
+     (make-instruction-sequence '(env) (list target)
+      `((assign ,target
+                (op ,op)
+                (const ,exp)
+                (reg env)))))))
+
+; assignment
 (define (compile-assignment exp target linkage compile-env)
-  (let ((var (assignment-variable exp))
-        (get-value-code
-         (compile (assignment-value exp) 'val 'next compile-env)))
-    (let ((address (find-variable var compile-env)))
-      (end-with-linkage
-       linkage
-       (preserving '(env)
-                   get-value-code
-                   (make-instruction-sequence
-                    '(env val)
-                    (list target)
-                    (list (if (eq? address 'not-found)
-                              (list (list perform
-                                          '(op set-variable-value!)
-                                          (list 'const var)
-                                          '(reg val)
-                                          '(reg env)))
-                              (list (list perform
-                                          '(op lexical-address-set!)
-                                          (list 'const address)
-                                          '(reg val)
-                                          '(reg env))))
-                          (list 'assign target '(const ok)))))))))
+  (let* ((var (assignment-variable exp))
+         (get-value-code
+          (compile (assignment-value exp) 'val 'next))
+         (address (find-variable var compile-env))
+         (op 'set-variable-value!))
+    (if (not (eq? address 'not-found))
+        (begin (set! op 'lexical-address-set!)
+               (set! var address)))
+    (end-with-linkage linkage
+     (preserving '(env)
+      get-value-code
+      (make-instruction-sequence '(env val) (list target)
+       `((perform (op ,op)
+                  (const ,var)
+                  (reg val)
+                  (reg env))
+         (assign ,target (const ok))))))))
